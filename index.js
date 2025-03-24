1d92d68d4ce3ee4e6c162bdeab34d565cad48e91
Nehemia Fortuna <gamerfornite19@gmail.com>
2025-03-24 18:53:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+// Importaciones de funciones
+const { addTask, listTasks, removeTask,updateTask } = require("./tasks");
+
+//aqui ambos metodos son iguales
+//send puedes enviar cualquier tipo de datos: texto,JSON, HTML lo que usted desee.
+//en cuanto a JSON envia el archivo en formato json pero limpio, es similar pero para respuestas en formato json.    
+app.get("/tasks", (req, res) =>{
+const ListaTareas = listTasks();    
+res.json(task);
+
+});
+
+
+//es para enviar datos del cliente al servidor.
+//por ejemplo si queremos que el inicie sesion, si queremos que el envie alguna consulta.
+//en este caso le estamos enviando un dato al servidor de que queremos agregar una tarea.
+//tambien es necesario usar req.body para guardar lo que envia en solicitud a POST.
+app.post("/tasks", (req, res) =>{
+const { task } = req.body //guarda lo que envio el usuario
+const resultado = addTask(task);
+if(!resultado)
+    {
+    return res.status(404).json({ message: "Ocurrio un error al añadir una tarea"}); // obtenemos el resultado de la consulta para añadir una tarea
+    }
+return res.status(200).json({ messsage: "Tarea agregada correctamente"});
+});
+
+//La función parseInt() convierte un texto a número entero.
+// por ejemplo:
+/*parseInt("3", 10); // Devuelve 3 (número)
+parseInt("10", 10); // Devuelve 10 (número)
+*/
+//,10 significa que el número es en base decimal (normal, base 10). Siempre ponlo así.
+//¿Por qué es necesario convertirlo?
+//Porque si no lo conviertes, JavaScript trataría el número como texto. 
+app.delete("/tasks:index", (req, res) => {
+const index = parseInt(req.params.index, 10);
+const resultadodelete = removeTask(index);
+if(!resultadodelete)
+    {
+    return res.status(404).json({ message: "Ocurrio un error al eliminar una tarea"});
+    }
+return res.status(200).json({ message: "Tarea eliminada con exito"});
+});
+
+//la petición PUT se usa para reemplazar todos los datos de un recurso
+//en este caso usamos funciones anteriores y metodos anteriores como el req.body.
+app.put("/tasks/:index", (req, res) => {
+const index = parseInt(req.params.index, 10);
+const { nuevaTarea } = req.body;
+const resultadoNuevaT = updateTask(index, nuevaTarea);
+if(!resultadoNuevaT)
+    {
+    return res.status(404).json({ message: "No se pudo actualiza la tarea"});
+    }
+return res.status(200).json({ message: "Tarea actualizada con exito"});
+});
---
+++ tasks.js
@@ -0,0 +1,55 @@
+// Variables y constantes
+const tasks = [];
+const MAX_TASKS = 5; // Constante
+
+// Función para agregar una tarea
+function addTask(task) {
+    if (tasks.length >= MAX_TASKS) {
+        console.log("No puedes agregar más tareas. Límite alcanzado.");
+        return false;
+    }
+    tasks.push(task);
+    console.log(`Tarea "${task}" agregada.`);
+    return true;
+}
+
+// Función para listar todas las tareas
+function listTasks() {
+    console.log("Lista de tareas:");
+    if (tasks.length === 0) {
+        console.log("No hay tareas.");
+    } else {
+        tasks.forEach((task, index) => {
+            console.log(`${index + 1}. ${task}`);
+        });
+    }
+}
+
+// Función para eliminar una tarea por su índice
+function removeTask(index) {
+    if (index < 0 || index >= tasks.length) {
+        console.log("Índice inválido.");
+        return false;
+    }
+    const removed = tasks.splice(index, 1);
+    console.log(`Tarea "${removed}" eliminada.`);
+    return true;
+}
+
+//funcion de actualizar tarea
+function updateTask(index, newTasks) {
+
+    if (index < 0 || index >= tasks.length){
+
+        console.log("indice invalido.");
+        return false;
+
+    }
+    tasks[index] = newTasks;
+    console.log(`Tarea actualizada: ${index + 1}. ${newTasks}`);
+    return false;
+
+}
+
+// Exportaciones de funciones
+module.exports = { addTask, listTasks, removeTask, updateTask };
